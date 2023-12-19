3a998526ba16d3a92b2d93f0dbe4e28073e0ff80
Mohammad7558 <hafezmohammod0011@gmail.com>
2023-12-19 13:47:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// practice Problem 1 === solve
+var momsGivesMoney = 1000;
+var orangePrice = 400;
+var applePrice = 300;
+
+var shopKeeperTake = orangePrice + applePrice;
+
+var shopKeeperReturn = momsGivesMoney - shopKeeperTake;
+console.log(shopKeeperReturn);
+
+
+// practice Problem 2 === solve
+var bangle = 75.25;
+var urdu = 65;
+var farsi = 80;
+var math = 35.45;
+var tesri = 99.50;
+
+var sum = bangle + urdu + farsi + math + tesri;
+var average = sum / 5;
+average = parseFloat(average.toFixed(2));
+console.log(average);
+
+
+
+// practice Problem 3 === solve
+var teacherSaid = 'I am going to be';
+var combine = 'an awesome web developer';
+
+var combined = teacherSaid+ ' ' +combine;
+console.log(combined);
+
+
+// practice Problem 4 === solve
+var sarahMomSaid = 119;
+var dividedByNumber = 5;
+
+var equal = sarahMomSaid % dividedByNumber;
+console.log(equal);
